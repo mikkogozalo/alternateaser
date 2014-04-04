416518e633b0e722af0b450ae17dfee79d858f18
Mikko Gozalo <conradmiguelegozalo@gmail.com>
2014-04-04 16:00:04 +0800

    refreshing stuff

---
+++ pyteaser_custom2/blah.py
@@ -0,0 +1,4 @@
+from pyteaser import SummarizeUrl
+url = 'http://www.huffingtonpost.com/2013/11/22/twitter-forward-secrecy_n_4326599.html'
+summaries = SummarizeUrl(url)
+print summaries
---
+++ pyteaser_custom2/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+
+
+setup(name='pyteaser',
+      version='1.0',
+      description="PyTeaser takes any news article and extract a brief summary from it",
+      license='MIT',
+      install_requires=['Pillow', 'lxml', 'cssselect', 'jieba', 'beautifulsoup'],
+      packages=find_packages(),
+      py_modules=['pyteaser'],
+      package_data={'goose': ['resources/images/*', 'resources/text/*']},
+      author = 'Xiao Xu',
+      author_email = 'xx56@cornell.edu',
+      url = 'https://github.com/xiaoxu193/PyTeaser',
+      test_suite='tests')
